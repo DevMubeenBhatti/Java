dffe1de17370f48b4a72f02bdcf130854ea04157
DevMubeenBhatti <mubeen.bhatti@systemsltd.com>
2021-11-18 13:25:32 +0500

    add array list

--- mvn projects/doctors/doctor/src/main/java/com/api/doctor/services/DoctorServices.java
+++ mvn projects/doctors/doctor/src/main/java/com/api/doctor/services/DoctorServices.java
@@ -1,15 +1,36 @@
 package com.api.doctor.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.api.doctor.model.Doctor;
 
 public class DoctorServices {
 	
 	public void saveDoctor() {
+		List<Doctor> doctorList= new ArrayList<Doctor>();
 		Doctor doctor = new Doctor();
 		doctor.setId(1);
 		doctor.setName("Muhammad Mubeen");
+		doctorList.add(doctor);
 		
-		System.out.println("ID : " + doctor.getId());
-		System.out.println("Name : " + doctor.getName());
+		Doctor doctor2 = new Doctor();
+		doctor2.setId(2);
+		doctor2.setName("Adnan");
+		doctorList.add(doctor2);
+		
+		Doctor doctor3 = new Doctor();
+		doctor3.setId(3);
+		doctor3.setName("Ali");
+		doctorList.add(doctor3);
+		
+		showList(doctorList);
+	}
+	
+	public void showList(List<Doctor> doctorList) {
+		for(Doctor doctor: doctorList) {
+			System.out.println("ID : " + doctor.getId());
+			System.out.println("Name : " + doctor.getName());
+		}
 	}
 }
